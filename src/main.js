05b6811a179a62af6d0ef48560d05bce6dc32b4e
kamemae <makaron@makaron.xyz>
2025-09-11 17:17:16 +0200

    3rd

--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import { Game } from "phaser";
-import { Preloader } from "./preloader";
-import { GameOverScene } from "./scenes/GameOverScene";
-import { HudScene } from "./scenes/HudScene";
-import { MainScene } from "./scenes/MainScene";
-import { MenuScene } from "./scenes/MenuScene";
-import { SplashScene } from "./scenes/SplashScene";
 
-// More information about config: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config = {
     type: Phaser.AUTO,
     parent: "phaser-container",
@@ -29,14 +22,7 @@ const config = {
             gravity: { y: 0 }
         }
     },
-    scene: [
-        Preloader,
-        SplashScene,
-        MainScene,
-        MenuScene,
-        HudScene,
-        GameOverScene
-    ]
+    scene: [  ]
 };
 
 new Game(config);
